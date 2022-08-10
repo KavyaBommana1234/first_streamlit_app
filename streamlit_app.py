f1cbb84041d292df21c87af18ee98cda3cf15895
KavyaBommana <110984966+KavyaBommana1234@users.noreply.github.com>
2022-08-10 20:30:28 +0530

    Update streamlit_app.py
    added subtitle and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.text('My Parents New Healthy Diner')
+streamlit.subtitle('Breakfast Menu')
+streamlit.text('Some xyz meals')
